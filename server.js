8e36f5ff3567ab9a78f5fd77555a97d2c8eb412d
lowlyHero <e.nunn1009@gmail.com>
2019-06-24 19:16:19 -0400

    deleted unused DELETE operation

--- server.js
+++ server.js
@@ -73,17 +73,6 @@ server.put('/accounts/:id', async (req, res) => {
 
 // ============ DELETE =================
 
-server.delete('/accounts', async (req, res) => {
-    try {
-        const accounts = await Accounts.remove(req.query);
-        res.status(200).json(accounts);
-    } catch(error) {
-        res.status(500).json({
-            message: 'Internal Error. Cannot remove account.'
-        });
-    }
-});
-
 server.delete('/accounts/:id', async (req, res) => {
     try {
         const accountId = await Accounts.remove(req.params.id);
