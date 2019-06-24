1a3b93aa4568ffbbedd683ffeac2e8a978d7e397
lowlyHero <e.nunn1009@gmail.com>
2019-06-24 17:22:26 -0400

    Added all CRUD operations in server.js. Just need to tweak small parts to make DB operational

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ const Accounts = require('./data/accounts-model');
 
 const server = express();
 
-// Within server.js add CRUD endpoints for the account resource. You may use data/accounts-model.js for access to your newly created database. The methods included in the accounts-model are described above in the Database Access section.
-// Use these endpoints to manually test that your database is working as expected.
+// ============ GET ===============
 
 server.get('/', async (req, res) => {
     try {
-        const accounts = await Accounts.find();
+        const accounts = await Accounts.find(req.query);
         res.status(200).json(accounts);
     } catch(error) {
         res.status(500).json({
@@ -20,6 +19,78 @@ server.get('/', async (req, res) => {
 server.get('/:id', async (req, res) => {
     try {
         const accountId = await Accounts.findById(req.params.id);
+        res.status(200).json(accountId);
+    } catch(error) {
+        res.status(500).json({
+            message: 'Internal Error. Cannot retrieve accounts.'
+        });
+    }
+});
+
+// ============ POST ===================
+
+server.post('/', async (req, res) => {
+    try {
+        const accounts = await Accounts.add(req.query);
+        res.status(201).json(accounts);
+    } catch(error) {
+        res.status(500).json({
+            message: 'Internal Error. Cannot retrieve accounts.'
+        });
+    }
+});
+
+server.post('/:id', async (req, res) => {
+    try {
+        const accountId = await Accounts.add(req.params.id);
+        res.status(201).json(accountId);
+    } catch(error) {
+        res.status(500).json({
+            message: 'Internal Error. Cannot retrieve accounts.'
+        });
+    }
+});
+
+// ============= PUT ===================
+
+server.put('/', async (req, res) => {
+    try {
+        const accounts = await Accounts.update(req.query);
+        res.status(200).json(accounts);
+    } catch(error) {
+        res.status(500).json({
+            message: 'Internal Error. Cannot retrieve accounts.'
+        });
+    }
+});
+
+server.put('/:id', async (req, res) => {
+    try {
+        const accountId = await Accounts.update(req.params.id);
+        res.status(200).json(accountId);
+    } catch(error) {
+        res.status(500).json({
+            message: 'Internal Error. Cannot retrieve accounts.'
+        });
+    }
+});
+
+// ============ DELETE =================
+
+server.delete('/', async (req, res) => {
+    try {
+        const accounts = await Accounts.remove(req.query);
+        res.status(200).json(accounts);
+    } catch(error) {
+        res.status(500).json({
+            message: 'Internal Error. Cannot retrieve accounts.'
+        });
+    }
+});
+
+server.delete('/:id', async (req, res) => {
+    try {
+        const accountId = await Accounts.remove(req.params.id);
         res.status(200).json(accountId);
     } catch(error) {
         res.status(500).json({
