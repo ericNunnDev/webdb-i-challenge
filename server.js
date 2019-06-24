5767b1c6ed6f99d7160efc9f500238b37fb8fd57
lowlyHero <e.nunn1009@gmail.com>
2019-06-24 14:25:17 -0400

    Initial Commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const server = express();
 
-// your code here
+// Within server.js add CRUD endpoints for the account resource. You may use data/accounts-model.js for access to your newly created database. The methods included in the accounts-model are described above in the Database Access section.
+// Use these endpoints to manually test that your database is working as expected.
 
 module.exports = server;
