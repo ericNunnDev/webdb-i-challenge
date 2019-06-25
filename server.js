8d5d67fce62745669ee82d5c9eae69f58276dc24
lowlyHero <e.nunn1009@gmail.com>
2019-06-25 14:16:46 -0400

    All CRUD operations functional

--- server.js
+++ server.js
@@ -49,21 +49,17 @@ server.post('/accounts', async (req, res) => {
 
 // ============= PUT ===================
 
-server.put('/accounts', async (req, res) => {
-    try {
-        const accounts = await Accounts.update(req.query);
-        res.status(200).json(accounts);
-    } catch(error) {
-        res.status(500).json({
-            message: 'Internal Error. Cannot update account.'
-        });
-    }
-});
-
 server.put('/accounts/:id', async (req, res) => {
     try {
-        const accountId = await Accounts.update(req.params.id);
-        res.status(200).json(accountId);
+        const account = await Accounts.update(req.params.id, req.body);
+
+        if(account) {
+            res.status(200).json(account);
+        } else {
+            res.status(404).json({
+                message: 'The account with the specified ID does not exist.'
+            })
+        }
     } catch(error) {
         res.status(500).json({
             message: 'Internal Error. Cannot update account.'
@@ -75,11 +71,21 @@ server.put('/accounts/:id', async (req, res) => {
 
 server.delete('/accounts/:id', async (req, res) => {
     try {
-        const accountId = await Accounts.remove(req.params.id);
-        res.status(200).json(accountId);
+        const accounts = await Accounts.remove(req.params.id);
+
+        if(accounts) {
+            res.status(200).json({
+                message: 'Account has been successfully deleted.',
+                accounts
+            });
+        } else {
+            res.status(404).json({
+                message: 'The account with the specified ID does not exist.'
+            })
+        }
     } catch(error) {
-        res.status(500).json({
-            message: 'Internal Error. Cannot remove account.'
+            res.status(500).json({
+                message: 'Internal Error. Cannot remove account.'
         });
     }
 });
