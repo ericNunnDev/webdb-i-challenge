d2bbe5266d4684ff1343197a5e5c55f3e212b066
lowlyHero <e.nunn1009@gmail.com>
2019-07-15 14:33:00 -0400

    Initial commit. Deps downloaded and serverjs file templated for CRUD ops

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const express = require('express');
+const db = require('./data/accounts-model');
 
 const server = express();
+server.use(express.json());
 
-// your code here
+server.get('/', async (req, res) => {
+
+}); 
+
+server.get('/:id', async (req, res) => {
+    
+}); 
+
+server.post('/', async (req, res) => {
+    
+}); 
+
+server.put('/:id', async (req, res) => {
+    
+}); 
+
+server.delete('/:id', async (req, res) => {
+    
+}); 
 
 module.exports = server;
