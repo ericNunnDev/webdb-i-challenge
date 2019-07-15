46a9e5f2663eafeb6f92ecc1695b7029f7da7787
lowlyHero <e.nunn1009@gmail.com>
2019-07-15 18:19:36 -0400

    serverjs functional. Still need to add data in

--- server.js
+++ server.js
@@ -2,26 +2,67 @@ const express = require('express');
 const db = require('./data/accounts-model');
 
 const server = express();
+
 server.use(express.json());
 
-server.get('/', async (req, res) => {
+server.get('/accounts', async (req, res) => {
+    try {
+        const accounts = await db.find(req.query);
+        res.status(200).json(accounts);
+    } catch(error) {
+        res.status(500).json({ message: 'Internal Error. Cannot retrieve accounts.' });
+    }
+});
 
-}); 
+server.get('/accounts/:id', async (req, res) => {
+    try {
+        const accountId = await db.findById(req.params.id);
+        res.status(200).json(accountId);
+    } catch(error) {
+        res.status(500).json({ message: 'Internal Error. Cannot retrieve accounts.' });
+    }
+});
 
-server.get('/:id', async (req, res) => {
-    
-}); 
+server.post('/accounts', async (req, res) => {
+    try {
+        let newAccount = {
+            name: req.body.name,
+            budget: req.body.budget
+        };
+        let createdAccId = await db.add(newAccount);
+        let createdAcc = await db.findById(createdAccId.id);
+        res.status(201).json(createdAcc);
+    } catch(error) {
+        res.status(500).json({ message: 'Internal Error. Cannot add account information.' });
+    }
+});
 
-server.post('/', async (req, res) => {
-    
-}); 
+server.put('/accounts/:id', async (req, res) => {
+    try {
+        const account = await db.update(req.params.id, req.body);
 
-server.put('/:id', async (req, res) => {
-    
-}); 
+        if(account) {
+            res.status(200).json(account);
+        } else {
+            res.status(404).json({ message: 'The account with the specified ID does not exist.' });
+        }
+    } catch(error) {
+        res.status(500).json({ message: 'Internal Error. Cannot update account.' });
+    }
+});
 
-server.delete('/:id', async (req, res) => {
-    
-}); 
+server.delete('/accounts/:id', async (req, res) => {
+    try {
+        const accounts = await db.remove(req.params.id);
+
+        if(accounts) {
+            res.status(200).json({ message: 'Account has been successfully deleted.', accounts });
+        } else {
+            res.status(404).json({ message: 'The account with the specified ID does not exist.' });
+        }
+    } catch(error) {
+            res.status(500).json({ message: 'Internal Error. Cannot remove account.' });
+    }
+});
 
 module.exports = server;
